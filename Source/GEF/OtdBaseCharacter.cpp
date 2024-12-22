c361c823a6857606a75d0d3925fb21539e03fcd2
Ádám Knapecz <knapecz.adam@gmail.com>
2024-12-22 16:33:15 +0100

    feat: bind inputs, teleport player after falling off the world

---
+++ Source/GEF/OtdBaseCharacter.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OtdBaseCharacter.h"
+
+// Sets default values
+AOtdBaseCharacter::AOtdBaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOtdBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	OnFellOutOfWorld();
+	
+}
+
+// Called every frame
+void AOtdBaseCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AOtdBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
